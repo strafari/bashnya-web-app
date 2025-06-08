ef6cb2bf1c0ff728e8e2962801af99f924a3ccad
strafari <punpum1@yandex.ru>
2025-06-08 21:58:21 +0700

    ащ

--- app-front/postcss.config.mjs
+++ app-front/postcss.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
+    autoprefixer: {},
   },
-};
+}
 
 export default config;
