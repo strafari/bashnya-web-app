80a07384946072f65e5c8c92f18a5ef22a4cceb0
strafari <punpum1@yandex.ru>
2025-06-08 20:41:56 +0700

    fix

--- app-front/src/app/layout.tsx
+++ app-front/src/app/layout.tsx
@@ -3,25 +3,25 @@ import { ReactNode } from "react";
 import { Montserrat, Unbounded } from 'next/font/google';
 import ClientLayout from "./ClientLayout";
 import AuthProvider from "../components/AuthProvider";
-export const metadata = {
-  title: "Башня ",
-  description:
-    "Профессиональная забота о питомцах в Норильске: «Хакуна Матата» — диагностика, лечение и вакцинация. Оставьте заявку онлайн!",
-  applicationName: "Хакуна Матата",
-  keywords: [
-    "ветклиника норильск",
-    "хакуна матата",
-    "ветеринарная клиника",
-    "ветеринар",
-    "лечение кошек",
-    "ветеринарные услуги",
-    "хакуна матата норильск",
-  ],
-  icons: {
-    icon: "/favicon.png",
-    shortcut: "/favicon.png",
-  },
-};
+// export const metadata = {
+//   title: "Башня ",
+//   description:
+//     "Профессиональная забота о питомцах в Норильске: «Хакуна Матата» — диагностика, лечение и вакцинация. Оставьте заявку онлайн!",
+//   applicationName: "Хакуна Матата",
+//   keywords: [
+//     "ветклиника норильск",
+//     "хакуна матата",
+//     "ветеринарная клиника",
+//     "ветеринар",
+//     "лечение кошек",
+//     "ветеринарные услуги",
+//     "хакуна матата норильск",
+//   ],
+//   icons: {
+//     icon: "/favicon.png",
+//     shortcut: "/favicon.png",
+//   },
+// };
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -34,17 +34,27 @@ const unbounded = Unbounded({
   display: 'swap',
 });
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="ru">
-      <head>
-        <meta name="yandex-verification" content="6b302a6f52754f1c" />
-      </head>
-      <body className="min-h-screen flex flex-col">
-        <AuthProvider>
-          <ClientLayout>{children}</ClientLayout>
-        </AuthProvider>
-      </body>
+    <html lang="ru" className={${montserrat.variable} ${unbounded.variable}}>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
+
+// export default function RootLayout({ children }: { children: ReactNode }) {
+//   return (
+//     <html lang="ru">
+//       <head>
+//         <meta name="yandex-verification" content="6b302a6f52754f1c" />
+//       </head>
+//       <body className="min-h-screen flex flex-col">
+//         <AuthProvider>
+//           <ClientLayout>{children}</ClientLayout>
+//         </AuthProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
+
