4c2fa40febeed13e0a5b733a774744dbcc1b0213
strafari <punpum1@yandex.ru>
2025-06-09 22:02:29 +0700

    Update next.config.js

--- app-front/next.config.js
+++ app-front/next.config.js
@@ -1,16 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+module.exports = {
   async rewrites() {
     return [
+      {
+        source: '/api/login',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/auth/jwt/login`,
+      },
       {
         source: '/api/:path*',
         destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
-    ]
+    ];
   },
-}
-
-module.exports = nextConfig
+};
