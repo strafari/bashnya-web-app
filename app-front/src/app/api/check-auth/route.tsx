215e4e13964eacd7e1dcad6a0d68dd147df2fabb
strafari <punpum1@yandex.ru>
2025-06-09 20:31:16 +0700

    Update route.tsx

--- app-front/src/app/api/check-auth/route.tsx
+++ app-front/src/app/api/check-auth/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-
+export const dynamic = 'force-dynamic';
 const RAW = process.env.NEXT_PUBLIC_API_URL || "";
 const API = RAW.endsWith("/") ? RAW.slice(0, -1) : RAW;
 
